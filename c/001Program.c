557ae297651a33be66e66ffa05a056a7c1430f93
albin-joseph <albinjoseph007@gmail.com>
2025-10-02 11:04:38 +0530

    add first c program

---
+++ c/001Program.c
@@ -0,0 +1,17 @@
+// This is a simple C program that prints "Hello, World!" to the console.
+// It uses the standard C library for input and output functions.
+// The program starts execution from the main function, which is the entry point of any C program.
+// The return value of 0 indicates that the program has executed successfully.
+// This code is compliant with the C17 language standard version.
+// To compile and run this program, you can use a C compiler like GCC:
+// gcc/clang 001Program.c -o 001Program
+// ./001Program
+// This will display the output "Hello, World!" in the terminal.
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
