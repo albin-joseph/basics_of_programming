f6966591214a05943419c46d576a171d7d4139c5
albin-joseph <albinjoseph007@gmail.com>
2025-10-04 09:48:08 +0530

    add find largest number in a 1D array

---
+++ c/array/easy/largestNumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int numbers[12] = {12, 21, 14, 2, 9, 10, 11, 22, 35, 3, 6, 8};
+    int largest_number = numbers[0];
+    for (int i=0;i<12; i++){
+        if(largest_number < numbers[i]) {
+            largest_number = numbers[i];
+        }
+    }
+
+    printf("Largest number in the array is: %d\n", largest_number);
+}
