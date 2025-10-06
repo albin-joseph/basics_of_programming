e04d51900ccb7fd6191f43fd925fe037792cfe12
albin-joseph <albinjoseph007@gmail.com>
2025-10-06 09:01:04 +0530

    update the sum of array program

--- c/array/easy/sumOfAllElement.c
+++ c/array/easy/sumOfAllElement.c
@@ -7,6 +7,10 @@ int main() {
     printf("Sum of all elements in an array\n");
     printf("Enter size of the array:\n");
     scanf("%d", &sizeOfArray);
+    if(sizeOfArray<1){
+        printf("Invalid array size. Please eneter a number 1 or above.\n");
+        return 0;
+    }
     printf("Enter elements:");
     int arryaX[sizeOfArray];
     for(int i=0; i<sizeOfArray; i++){
